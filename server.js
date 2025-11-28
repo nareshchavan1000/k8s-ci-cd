2d83b1acaeabc20d0ed850a85e712f955da4a060
Naresh Chavan <nareshchavan1000@gmail.com>
2025-11-28 17:40:48 +0530

    added name

--- server.js
+++ server.js
@@ -10,12 +10,15 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/',(req,res)=>{
     res.send('Hello World...!!');
+    res.send('Naresh Chavan');
 });
 
 app.get('/next',(req,res)=>{
     res.send("Next route...");
     res.send("No worries");
 });
+
+
 
 app.listen((PORT),()=>{
     console.log("Server Started at port "+PORT);
